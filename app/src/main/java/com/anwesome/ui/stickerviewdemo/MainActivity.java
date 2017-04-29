ecca898f4b71f2f07fcd4b30a062bb1243aa4760
Anwesh43 <anweshthecool0@gmail.com>
2017-04-29 11:06:03 +0530

    Using StickerView in your app

--- app/src/main/java/com/anwesome/ui/stickerviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/stickerviewdemo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.anwesome.ui.stickerviewdemo;
 
+import android.content.pm.ProviderInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.anwesome.ui.stickerview.StickerLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<Bitmap> bitmaps = new ArrayList<>();
+    private int images[] = {R.drawable.sml1,R.drawable.sml2,R.drawable.sml3};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StickerLayout stickerLayout = new StickerLayout(this);
+        if(bitmaps.size() == 0) {
+            for(int image:images) {
+                bitmaps.add(BitmapFactory.decodeResource(getResources(),image));
+            }
+        }
+        stickerLayout.setStickers(bitmaps);
+        stickerLayout.setMainImage(BitmapFactory.decodeResource(getResources(),R.drawable.back3));
+        stickerLayout.show();
     }
 }
